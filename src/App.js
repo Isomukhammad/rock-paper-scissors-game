8ad19bab01ce72e47bd918c5b9b06d27b380767d
Isomukhammad <s.isomuxammad@gmail.com>
2022-09-07 10:02:32 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import Scoreboard from './components/scoreboard/scoreboard.component';
+import { useSelector } from 'react-redux';
 
-import styles from './App.module.css';
+import { selectAction } from './store/score/score.selector';
+
+import Scoreboard from './components/scoreboard/scoreboard.component';
 import Buttons from './components/buttons/buttons.component';
 import RulesButton from './components/rules-button/rules-button.component';
-import { useSelector } from 'react-redux';
-import { selectAction, selectOpponentChoice } from './store/score/score.selector';
 import Result from './components/result/result.component';
-import { useEffect } from 'react';
+
+import styles from './App.module.css';
 
 const App = () => {
   const action = useSelector(selectAction);
-  const opponentChoice = useSelector(selectOpponentChoice)
-
-  useEffect(() => {
-    console.log(opponentChoice)
-  }, [opponentChoice])
 
   return(
     <div className={styles.div}>
